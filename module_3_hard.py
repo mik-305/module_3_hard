59aa4cd81b4aa70f069d7484e8fd44a870566ad1
mik-305 <mik-305@bk.ru>
2024-08-16 17:27:24 +0300

    Add files via upload

--- module_3_hard.py
+++ module_3_hard.py
@@ -3,15 +3,14 @@ def calculate_structure_sum(data_structure):
 
 
 data_structure = [[1, 2, 3], {'a': 4, 'b': 5}, (6, {'cube': 7, 'drum': 8}), "Hello",
-                  ((), [{(2, 'Urban', ('Urban2', 35))}])
-                  ]
+                  ((), [{(2, 'Urban', ('Urban2', 35))}])]
 
 for i in range(len(data_structure)):  # Перечисление по количеству входных элементов списка
-    elem_spi = data_structure[i]      # Получаем по очереди элемент списка
+    elem_spi = data_structure[i]  # Получаем по очереди элемент списка
     print(type(elem_spi), '--', elem_spi, 'Длина элемента -', len(elem_spi))
-    # print('Длина элемента -',len(data_structure[i]))
-    # print(range(len(element)))
-    # for j in range(len(element)):
-    # print(element[j])
-
-# 'Тип элемента -',
+    if isinstance(elem_spi,str):
+        print('Это строка -', elem_spi)
+    if isinstance(elem_spi,dict):
+        print('Это словарь -', elem_spi)
+    if isinstance(elem_spi,dict):
+        print('Это словарь -', elem_spi)
