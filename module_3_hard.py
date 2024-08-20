234c831c92afc23618175a6387da34555a456b8d
mik-305 <mik-305@bk.ru>
2024-08-20 08:57:49 +0300

    Add files via upload

--- module_3_hard.py
+++ module_3_hard.py
@@ -1,16 +1,18 @@
-def calculate_structure_sum(data_structure):
-    return
-
+def calculate_nested_sum(data_structure):
+    total_sum = 0
+    if isinstance(data_structure,(list, tuple, set)):            # Если список, кортеж, множество
+        for item in data_structure:
+           total_sum = total_sum + calculate_nested_sum(item)
+    elif isinstance(data_structure, dict):                      # Если словарь
+        for key, value in data_structure.items():               # Разбираем словарь на ключ и значение
+            total_sum = total_sum + calculate_nested_sum(key)
+            total_sum = total_sum + calculate_nested_sum(value)
+    elif isinstance(data_structure, int):                       # Если целое число
+        total_sum = total_sum + data_structure
+    elif isinstance(data_structure, str):                       # Если строка
+        total_sum = total_sum + len(data_structure)
+    return total_sum
 
 data_structure = [[1, 2, 3], {'a': 4, 'b': 5}, (6, {'cube': 7, 'drum': 8}), "Hello",
                   ((), [{(2, 'Urban', ('Urban2', 35))}])]
-
-for i in range(len(data_structure)):  # Перечисление по количеству входных элементов списка
-    elem_spi = data_structure[i]  # Получаем по очереди элемент списка
-    print(type(elem_spi), '--', elem_spi, 'Длина элемента -', len(elem_spi))
-    if isinstance(elem_spi,str):
-        print('Это строка -', elem_spi)
-    if isinstance(elem_spi,dict):
-        print('Это словарь -', elem_spi)
-    if isinstance(elem_spi,dict):
-        print('Это словарь -', elem_spi)
+print('Итоговая сумма :',calculate_nested_sum(data_structure))
